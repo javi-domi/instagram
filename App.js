8348dd5860b5d585a65c35c072239e82a71634e2
Javier Dominguez <javdominguez@icloud.com>
2021-05-22 22:08:47 -0300

    App: implemented take/pick/save picture in database

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import LandingScreen from "./components/auth/Landing";
 import RegisterScreen from "./components/auth/Register";
 import MainScreen from "./components/Main";
 import AddScreen from "./components/main/Add";
+import SaveScreen from "./components/main/Save";
 
 import firebaseConfig from "./config";
 import firebase from "firebase/app";
@@ -85,7 +86,16 @@ export class App extends Component {
               component={MainScreen}
               options={{ headerShown: false }}
             />
-            <Stack.Screen name="Add" component={AddScreen} />
+            <Stack.Screen
+              name="Add"
+              component={AddScreen}
+              navigation={this.props.navigation}
+            />
+            <Stack.Screen
+              name="Save"
+              component={SaveScreen}
+              navigation={this.props.navigation}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
--- components/main/Add.js
+++ components/main/Add.js
@@ -1,42 +1,102 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 import { Camera } from "expo-camera";
-import { Button } from "react-native-paper";
+import * as ImagePicker from "expo-image-picker";
 
-export default function App() {
+export default function Add({ navigation }) {
   const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
       setHasPermission(status === "granted");
+
+      const { galleryStatus } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (galleryStatus === "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
     })();
   }, []);
 
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      setImage(data.uri);
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   if (hasPermission === null) {
-    return <View />;
+    return (
+      <View>
+        <Text>Que pasa?</Text>
+      </View>
+    );
   }
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
-        <Camera style={{ flex: 1 }} type={type} />
+      <View style={styles.cameraContainer}>
+        <Camera
+          ref={(ref) => setCamera(ref)}
+          style={styles.fixedRatio}
+          type={type}
+          ratio={"1:1"}
+        />
       </View>
-      <Button
-        title="Flip Image"
-        onPress={() => {
-          setType(
-            type === Camera.Constants.Type.back
-              ? Camera.Constants.Type.front
-              : Camera.Constants.Type.back
-          );
-        }}
-      ></Button>
+      <View>
+        <Button
+          style={styles.fixedRatio}
+          title="Flip Image"
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}
+        ></Button>
+        <Button title="Take picture" onPress={() => takePicture()} />
+        <Button title="Pick image" onPress={() => pickImage()} />
+        <Button
+          title="Save"
+          onPress={() => navigation.navigate("Save", { image })}
+        />
+      </View>
+      {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cameraContainer: {
+    flex: 1,
+    flexDirection: "row",
+    padding: "2%",
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1,
+    borderRadius: 16,
+  },
+});
